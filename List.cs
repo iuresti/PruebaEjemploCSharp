d152b83a0d54055aa96777dba95e1a1cb345191d
Ivan Uresti <iuresti@skytouchtechnology.com>
2023-03-17 09:11:05 -0600

    Add project files.

---
+++ List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaEjemplo
+{
+    interface List<G>
+    {
+        void addAtTail(G data);
+        void addAtFront(G data);
+        void remove(int index);
+        void removeAll();
+        void setAt(int index, G data);
+        G getAt(int index);
+        void removeAllWithValue(G data);
+        int getSize();
+        Iterator<G> getIterator();
+        bool isEmpty();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PruebaEjemplo
+{
+
+    class ArrayList <T>: List<T>
+    {
+        public void addAtTail(T data)
+        {
+
+        }
+
+        public void addAtFront(T data)
+        {
+
+        }
+
+        void remove(int index);
+        void removeAll();
+        void setAt(int index, T data);
+        T getAt(int index);
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
